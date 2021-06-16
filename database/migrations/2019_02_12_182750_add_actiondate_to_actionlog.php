b6fd2db3b2518433e1138796307909abe4faaf8e
Travis Miller <milletr@tulsaschools.org>
2021-06-16 15:11:08 -0500

    TPS: improve actiondate in actionlog migration performance

--- database/migrations/2019_02_12_182750_add_actiondate_to_actionlog.php
+++ database/migrations/2019_02_12_182750_add_actiondate_to_actionlog.php
@@ -17,11 +17,8 @@ class AddActionDateToActionlog extends Migration
             $table->datetime('action_date')->nullable()->default(null);
         });
 
-        $results = DB::table('action_logs')->select('created_at', 'id')->where('action_type', 'checkout')->orWhere('action_type', 'checkin from')->get();
-
-        foreach($results as $result){
-            $update = DB::update('update '.DB::getTablePrefix().'action_logs set action_date=? where id=?', [$result->created_at, $result->id]);
-        }
+        $prefix = DB::getTablePrefix();
+        DB::update("UPDATE ${prefix}action_logs SET action_date = created_at WHERE action_date IS NULL AND action_type IN ('checkout', 'checkin from')");
     }
 
     /**
